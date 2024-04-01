95b2fbd611865ee7b1fe80279e1847fc1c3c3677
haroldcutti <harold.cutti@tecsup.edu.pe>
2024-04-01 10:37:34 -0500

    A

---
+++ index.js
@@ -0,0 +1,21 @@
+const express =  require('express')
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hola mundo express')
+});
+
+app.get('/clientes', (req, res) => {
+    const clientes = ['Harold', 'Jesus', 'Jusepi'];
+    res.json(clientes);
+});
+
+app.get('/productos', (req, res) => {
+    const producto = ['Televisor', 'Licuadora', 'Teclado'];
+    res.json(producto);
+});
+
+const PORT = 5000;
+app.listen(PORT, () =>{
+    console.log('Se ejecuta la aplicación')
+});
